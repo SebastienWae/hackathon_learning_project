f76cfbe17a614d1ba7e3e99d4fb89cebbcccf6ce
Jenny Eulliot <jeulliot@k0r1p7.42mulhouse.fr>
2022-04-18 17:58:53 +0200

    jenny annalyns

---
+++ exercism/jenny/javascript/annalyns-infiltration/annalyns-infiltration.js
@@ -0,0 +1,21 @@
+
+export function canExecuteFastAttack(knightIsAwake) {
+ return(!knightIsAwake); 
+}
+
+export function canSpy(knightIsAwake, archerIsAwake, prisonerIsAwake) {
+  return(knightIsAwake || archerIsAwake || prisonerIsAwake);
+}
+
+export function canSignalPrisoner(archerIsAwake, prisonerIsAwake) {
+ return((!archerIsAwake) && prisonerIsAwake);
+}
+
+export function canFreePrisoner(
+  knightIsAwake,
+  archerIsAwake,
+  prisonerIsAwake,
+  petDogIsPresent
+) {
+  return((petDogIsPresent && !archerIsAwake) || (!petDogIsPresent && prisonerIsAwake && (!archerIsAwake && !knightIsAwake)));
+}
