6267579d5c1fd74b7557cb365631a58a0aa26923
Jenny Eulliot <jeulliot@k0r1p7.42mulhouse.fr>
2022-04-18 17:07:12 +0200

    exercism elyses jenny

---
+++ exercism/jenny/javascript/elyses-enchantments/enchantments.js
@@ -0,0 +1,39 @@
+
+export function getItem(cards, position) {
+  return(cards[position]);
+}
+
+export function setItem(cards, position, replacementCard) {
+  cards[position] = replacementCard;
+  return(cards);
+}
+
+export function insertItemAtTop(cards, newCard) {
+  cards.push(newCard);
+  return(cards);
+}
+
+export function removeItem(cards, position) {
+  cards.splice(position, 1);
+  return(cards);
+}
+
+export function removeItemFromTop(cards) {
+  cards.pop();
+  return(cards);
+}
+
+
+export function insertItemAtBottom(cards, newCard) {
+  cards.splice(0,0,newCard);
+  return(cards);
+}
+
+export function removeItemAtBottom(cards) {
+  cards.splice(0,1);
+  return(cards);
+}
+
+export function checkSizeOfStack(cards, stackSize) {
+  return(cards.length === stackSize);
+}
