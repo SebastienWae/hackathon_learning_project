a83872c0548df748b688e1786008ec46f135e4a1
Jean-michel Rasser <jrasser@k0r1p6.42mulhouse.fr>
2022-04-18 17:47:42 +0200

    jm poetry

---
+++ exercism/jm/javascript/poetry-club-door-policy/door-policy.js
@@ -0,0 +1,85 @@
+// @ts-check
+//
+// ☝🏽 The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion on the web
+// and supported IDEs when implementing this exercise. You don't need to
+// understand types, JSDoc, or TypeScript in order to complete this JavaScript
+// exercise, and can completely ignore this comment block and directive.
+//
+// 👋🏽 Hi again!
+//
+// A quick reminder about exercise stubs:
+//
+// 💡 You're allowed to completely clear any stub before you get started. Often
+// we recommend using the stub, because they are already set-up correctly to
+// work with the tests, which you can find in ./door-policy.spec.js.
+//
+// 💡 You don't need to write JSDoc comment blocks yourself; it is not expected
+// in idiomatic JavaScript, but some companies and style-guides do enforce them.
+//
+// Good luck with that door policy!
+
+import internal from "stream";
+
+/**
+ * Respond with the correct character, given the line of the
+ * poem, if this were said at the front door.
+ *
+ * @param {string} line
+ * @returns {string}
+ */
+export function frontDoorResponse(line) {
+  return line[0];
+}
+
+/**
+ * Format the password for the front-door, given the response
+ * letters.
+ *
+ * @param {string} word the letters you responded with before
+ * @returns {string} the front door password
+ */
+export function frontDoorPassword(word) {
+  let maj;
+  let res;
+
+  res = word.toLowerCase().slice(1);
+
+  maj = word[0].toUpperCase();
+
+
+  return (maj + res)
+}
+
+/**
+ * Respond with the correct character, given the line of the
+ * poem, if this were said at the back door.
+ *
+ * @param {string} line
+ * @returns {string}
+ */
+export function backDoorResponse(line) {
+  let res;
+
+  res = line.trim();
+  res = res[res.length - 1]
+  return (res)
+}
+
+/**
+ * Format the password for the back door, given the response
+ * letters.
+ *
+ * @param {string} word the letters you responded with before
+ * @returns {string} the back door password
+ * 
+ * backDoorPassword('horse');
+ * => "Horse, please"
+ */
+export function backDoorPassword(word) {
+  let res;
+
+  res = frontDoorPassword(word);
+  res += ", please";
+  return (res);
+}
