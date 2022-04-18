e4f079b2d5f61b9beef0c56fc48038ce6f640d69
Jean-michel Rasser <jrasser@k0r1p6.42mulhouse.fr>
2022-04-18 19:48:38 +0200

    jm birds

---
+++ exercism/jm/javascript/bird-watcher/bird-watcher.js
@@ -0,0 +1,53 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+import { format } from "path";
+
+/**
+ * Calculates the total bird count.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number} total bird count
+ */
+export function totalBirdCount(birdsPerDay) {
+  let count = 0;
+
+  for(let i = 0; i < birdsPerDay.length; i++)
+    count+=birdsPerDay[i] ;
+  return count;
+}
+
+/**
+ * Calculates the total number of birds seen in a specific week.
+ *
+ * @param {number[]} birdsPerDay
+ * @param {number} week
+ * @returns {number} birds counted in the given week
+ */
+export function birdsInWeek(birdsPerDay, week) {
+  let count = 0;
+  let week_tan = birdsPerDay.slice((week - 1) * 7, week * 7);
+
+  for(let i =0; i < week_tan.length; i++)
+    count += week_tan[i];
+  return (count)
+}
+
+/**
+ * Fixes the counting mistake by increasing the bird count
+ * by one for every second day.
+ *
+ * @param {number[]} birdsPerDay
+ * @returns {number[]} corrected bird count data
+ */
+export function fixBirdCountLog(birdsPerDay) {
+  for(let i = 0; i < birdsPerDay.length; i++)
+  {
+    if (i % 2 == 0)
+      birdsPerDay[i] += 1;
+  }
+  return (birdsPerDay)
+}
