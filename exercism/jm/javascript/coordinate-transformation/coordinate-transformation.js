246e2d0c40a8308bdf47d8444c2764f64b82fe16
Jean-michel Rasser <jrasser@k0r1p6.42mulhouse.fr>
2022-04-19 17:41:17 +0200

    jm coordinat

---
+++ exercism/jm/javascript/coordinate-transformation/coordinate-transformation.js
@@ -0,0 +1,86 @@
+// @ts-check
+//
+// The line above enables type checking for this file. Various IDEs interpret
+// the @ts-check directive. It will give you helpful autocompletion when
+// implementing this exercise.
+
+/**
+ * Create a function that returns a function making use of a closure to
+ * perform a repeatable 2d translation of a coordinate pair.
+ *
+ * @param {number} dx the translate x component
+ * @param {number} dy the translate y component
+ *
+ * @returns {function} a function which takes an x, y parameter, returns the
+ *  translated coordinate pair in the form [x, y]
+ */
+export function translate2d(dx, dy) {
+  const sub = (x, y) => ([x + dx, y + dy])
+  return (sub)
+}
+
+/**
+ * Create a function that returns a function making use of a closure to
+ * perform a repeatable 2d scale of a coordinate pair.
+ *
+ * @param {number} sx the amount to scale the x component
+ * @param {number} sy the amount to scale the y component
+ *
+ * @returns {function} a function which takes an x, y parameter, returns the
+ *  scaled coordinate pair in the form [x, y]
+ */
+export function scale2d(sx, sy) {
+  const f2 = function (x,y)
+  {
+    return ([x * sx, y * sy]);
+  }
+  return (f2);
+}
+
+/**
+ * Create a composition function that returns a function that combines two
+ * functions to perform a repeatable transformation
+ *
+ * @param {function} f the first function to apply
+ * @param {function} g the second function to apply
+ *
+ * @returns {function} a function which takes an x, y parameter, returns the
+ *  transformed coordinate pair in the form [x, y]
+ */
+export function composeTransform(f, g) {
+  const h = function (x, y)
+  {
+    let co = f(x, y);
+    return g(co[0], co[1]);
+  }
+  return (h)
+}
+
+/**
+ * Return a function that memoizes the last result.  If the arguments are the same as the last call,
+ * then memoized result returned.
+ *
+ * @param {function} f the transformation function to memoize, assumes takes two arguments 'x' and 'y'
+ *
+ * @returns {function} a function which takes x and y arguments, and will either return the saved result
+ *  if the arguments are the same on subsequent calls, or compute a new result if they are different.
+ */
+export function memoizeTransform(f) {
+  let mem;
+  let fct
+  let mem_x;
+  let mem_y;
+
+  const g = function (x, y)
+  {
+    if (x != mem_x || y != mem_y)
+    {
+      mem_x = x;
+      mem_y = y;
+      fct = f(x,y)
+      mem = [fct[0], fct[1]];
+    }
+    return (mem);
+  }
+  return (g)
+}
