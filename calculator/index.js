a30ada8246d2c13c5e623aecd39e52c0c27956dc
Jean-michel Rasser <jrasser@k0r1p6.42mulhouse.fr>
2022-04-20 21:23:18 +0200

    v1 fini

--- calculator/index.js
+++ calculator/index.js
@@ -8,40 +8,31 @@ try {
 		if (value != '=')
 			document.querySelector("#calcul").innerHTML += value;
 	}
-
 	const display_sum = function() {
 		document.querySelector("#sum").innerHTML = sum;
 	}
-
 	const update_sum = function(number) {
-		sum = parseInt(number);
+		sum = parseFloat(number);
 	}
-
 	const operate_sum = function(operator, number) {
-		//console.log( "number " + number + ", sum : "+ sum)
-		//if (operator != '')
-		//	console.log ( + ", operator :" + operator)
-		if (operator === '+') sum += parseInt(number);
-		if (operator === '-') sum -= parseInt(number);
-		if (operator === 'x') sum *= parseInt(number);
-		if (operator === '/') sum /= parseInt(number);
+		if (operator === '+') sum += parseFloat(number);
+		if (operator === '-') sum -= parseFloat(number);
+		if (operator === 'x') sum *= parseFloat(number);
+		if (operator === '/') sum /= parseFloat(number);
 		if (operator === '%') sum /= 100;
 	}
-
 	const update_values = function(values, n) {
 		for(let i = 0; i <= n; i++) {
 			values.shift();
 		}
 	}
-
 	const parse_values = function(values) {
-		//console.log("values avant: ", values);
 		let number = '';
 		for(let i = 0; i < values.length; i++) {
-			if (values[i] >= '0' && values[i] <= '9')
+			if (values[i] >= '0' && values[i] <= '9' || values[i] == '.')
 				number += values[i];
-			else if (values[i] == '+' || values[i] == '-' || values[i] == 'x'
-				|| values[i] == '/' || values[i] == '=' || values[i] == '%')
+			else if (i > 0 && (values[i] == '+' || values[i] == '-' || values[i] == 'x'
+				|| values[i] == '/' || values[i] == '=' || values[i] == '%'))
 			{
 				if (operator != '')
 					operate_sum(operator, number)
@@ -50,7 +41,6 @@ try {
 				operator = values[i];
 				update_values(values, i);
 				number = '';
-				console.log("sum : ", sum);
 				display_sum();
 			}
 			else if (values[i] == 'C')
@@ -61,53 +51,39 @@ try {
 				update_values(values, values.length);
 				document.querySelector("#calcul").innerHTML = '';
 				document.querySelector("#sum").innerHTML = 0;
-
 			}
-
 		}
-		//console.log("values apres: ", values);
-		//console.log("number: ", number);
-		//console.log("operator: ", operator);
-
 	}
-
 	const getInput = function (e) {
-
 		value = e.target.innerHTML;
 		if (e.target.innerHTML != "Del") {
+			
 			values.push(e.target.innerHTML);
-			console.log("values avant: " + values);
+			if (values.length >= 15)
+				alert("HAAAAAAAA");
 			display_calcul(values[values.length - 1]);
 			parse_values(values);
 		} 
 		else
 		{
-			console.log("values maintenann: avant : " + values);
-
 			if (values[0] == undefined)
 			{
 				operator = '';
-				console.log("values maintenant ICI: " + values);
 				for(let i = 0; i < sum.toString().length; i++) {
 					values.push(sum.toString()[i])
 				}
-
 			}
 			else
 				values.pop();
-				document.querySelector("#calcul").innerHTML = document.querySelector("#calcul").innerHTML.slice(0, -1);
+			document.querySelector("#calcul").innerHTML = document.querySelector("#calcul").innerHTML.slice(0, -1);
 		}
-		console.log("values apres: " + values);
-
 	}
-
-
 	buttons.forEach(b => {
 		b.addEventListener('click', getInput);
 	})
 }
 catch (error) {
-
+	throw new Error(error);
 }
 
 
