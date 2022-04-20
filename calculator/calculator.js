89417f589b5259a3c5a153220e8c191296be9710
Jean-michel Rasser <jrasser@k0r1p6.42mulhouse.fr>
2022-04-20 14:10:50 +0200

    structure calculator

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,10 +1,10 @@
-const buttons = document.querySelectorAll("button");               
-                
+const buttons = document.querySelectorAll("button");
+				
 for (let i = 0; i < buttons.length; i++) {
-    buttons[i].addEventListener("click", function () {
-        let j = buttons[i].innerHTML;
-        console.log(j);    
-        document.querySelector(".resultat").innerHTML += `${j}`;                    
-    });
+	buttons[i].addEventListener("click", function () {
+		let j = buttons[i].innerHTML;
+		console.log(j);
+		document.querySelector(".resultat").innerHTML += `${j}`;
+	});
 }  
 
