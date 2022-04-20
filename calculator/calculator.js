1bd1e9c1c050da4267cd1ec62c46932870260cbd
Jean-michel Rasser <jrasser@k0r1p6.42mulhouse.fr>
2022-04-20 14:56:37 +0200

    display center

--- calculator/calculator.js
+++ calculator/calculator.js
@@ -1,5 +1,8 @@
-const buttons = document.querySelectorAll("button");
-				
+const	buttons = document.querySelectorAll("button");
+const	display = '';
+
+console.log(buttons);
+
 for (let i = 0; i < buttons.length; i++) {
 	buttons[i].addEventListener("click", function () {
 		let j = buttons[i].innerHTML;
