3eaf4ce742cd9f9c05d302f17d81b06c0c586e7a
Jenny Eulliot <jeulliot@k0r1p6.42mulhouse.fr>
2022-04-21 21:20:06 +0200

    images

--- weather-jg/index.js
+++ weather-jg/index.js
@@ -36,6 +36,18 @@ function displayWeather(weatherContent) {
     document.querySelector("#tmp_act").innerHTML = 'Temperature actuelle : ' + weatherContent.data[0].temp;
     document.querySelector("#tmp_min").innerHTML = 'Temperature minimal : ' + weatherContent.data[0].min_temp;
     document.querySelector("#tmp_max").innerHTML = 'Temperature maximal : ' + weatherContent.data[0].max_temp;
+    let wth = weatherContent.data[0].weather.code;
+    console.log(wth);
+    if (wth>= 300 && wth <700)
+        document.getElementById("img_res").src="rain.png";
+    if (wth === 800 || wth === 801)
+        document.getElementById("img_res").src="sun.png";
+    if ((wth>= 700 && wth <800) || wth == 802)
+        document.getElementById("img_res").src="cloudish.png";
+    if (wth === 803 || wth === 804)
+        document.getElementById("img_res").src="cloud.png";
+    if (wth>= 200 && wth <300)
+        document.getElementById("img_res").src="thunder.png";
 }
 
 function saveAsFavorite() {
