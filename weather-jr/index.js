f4e938984886b2ba150a12842e24a422e35f9d6a
Jean-michel Rasser <jrasser@k0r1p7.42mulhouse.fr>
2022-04-21 22:03:26 +0200

    style

--- weather-jr/index.js
+++ weather-jr/index.js
@@ -5,20 +5,9 @@ const weatherInfo = document.querySelector('#weatherInfo');
 const defaultLogo = document.querySelector('#default-logo');
 const ul = document.querySelector('ul');
 const cityName = document.querySelector('#city-name');
-const option = {weekday: 'long', day: 'numeric', month: 'long'};
+const option = { weekday: 'long', day: 'numeric', month: 'long' };
 let city;
 let state = 0;
-
-function deleteChild(element) {
-    let elem = document.querySelector(element);
-    
-    let child = elem.lastChild;
-    while(child) {
-        elem.removeChild(child);
-        child = elem.lastChild;
-    }
-}
-
 let btnAddFavoris = document.getElementById("btnAddFavori");
 let btnFavori = document.querySelector(".favoris");
 let btnAsides = document.querySelector('aside');
@@ -26,18 +15,13 @@ let data1;
 
 function addFavori() {
     if (data1) {
-        console.log("storage avant: ", localStorage);
-        console.log("response : ", data1);
-
         localStorage.setItem(localStorage.length + 1, data1.city_name);
         displayFavori();
-        console.log("storage apres: ", localStorage);
     }
     else {
         console.log("Impossible de rajouter le favoris")
     }
 }
-
 function displayFavori() {
     btnFavori.innerHTML += `
     <button class="btnFavori">
@@ -45,11 +29,9 @@ function displayFavori() {
     </button>
     `
 }
-
-function displayFavoris(){
+function displayFavoris() {
     console.log("longeur localstorage : ", localStorage.length);
-    for(let i = 1; i <= localStorage.length; i++)
-    {
+    for (let i = 1; i <= localStorage.length; i++) {
         btnFavori.innerHTML += `
         <button class="btnFavori">
             ${localStorage[i]}
@@ -57,10 +39,18 @@ function displayFavoris(){
         `
     }
 }
+function deleteChild(element) {
+    let elem = document.querySelector(element);
 
+    let child = elem.lastChild;
+    while (child) {
+        elem.removeChild(child);
+        child = elem.lastChild;
+    }
+}
 function getSearch(e) {
-  
-  if (e.target.innerHTML == 'OK') {
+
+    if (e.target.innerHTML == 'OK') {
         city = document.getElementById('search').value;
     } else {
         city = e.target.innerHTML;
@@ -68,23 +58,23 @@ function getSearch(e) {
     const myRequest = fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city}&key=${API_KEY}`);
     myRequest.then(
         response => {
-             if (response.status === 200) {
-                    response.json().then(data => {
-                        
-                        if(ul.lastChild){
-                            deleteChild('ul');
-                        }
-                        for (let i = 0; i < 16; i++){
-                            const newli = document.createElement('li')
-                            const date = new Date(data['data'][String(i)]['datetime'])
-                            newli.innerHTML = date.toLocaleDateString("fr", option) + 
+            if (response.status === 200) {
+                response.json().then(data => {
+                    if (ul.lastChild) {
+                        deleteChild('ul');
+                    }
+                    for (let i = 0; i < 16; i++) {
+                        const newli = document.createElement('li');
+                        const date = new Date(data['data'][String(i)]['datetime']);
+                        newli.innerHTML = date.toLocaleDateString("fr", option) +
                             "<br><br>" + data['data'][String(i)]['temp'] + '°';
-                            ul.appendChild(newli);
-                        }
-                        data1 = data;
-                        defaultLogo.classList.add('hidden');
-                        cityName.textContent = city.toUpperCase();
-                    })
+                        console.log("ici : ", date)
+                        ul.appendChild(newli);
+                    }
+                    data1 = data;
+                    defaultLogo.classList.add('hidden');
+                    cityName.textContent = city.toUpperCase();
+                })
             } else {
                 cityName.textContent = 'Aucune ville ne correspond à votre requete ';
                 deleteChild('ul');
