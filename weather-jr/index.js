b8b31bea4fd4524a89f48bdf9c896ac9598e8eec
Reda Tazlaoui <rtazlaou@k0r1p9.42mulhouse.fr>
2022-04-21 19:26:38 +0200

    Add Request fonction and Actualisation City

--- weather-jr/index.js
+++ weather-jr/index.js
@@ -1,8 +1,50 @@
-let input;
-let btnSearch = document.getElementById("btnSearch");
-btnSearch.addEventListener('click', getSearch);
+const btnSearch = document.getElementById("btnSearch");
+const API_KEY = '1168561e15ab488e8915043a707022aa';
+const mainSection = document.querySelector('#main-section');
+const weatherInfo = document.querySelector('#weatherInfo');
+const defaultLogo = document.querySelector('#default-logo');
+const ul = document.querySelector('ul');
+const cityName = document.querySelector('#city-name');
+const option = {weekday: 'long', day: 'numeric', month: 'long'};
+let city;
+let state = 0;
+
+function deleteChild(element) {
+    let elem = document.querySelector(element);
+    
+    let child = elem.lastChild;
+    while(child) {
+        elem.removeChild(child);
+        child = elem.lastChild;
+    }
+}
 
 function getSearch(e) {
-    input = document.getElementById('search').value;
-    console.log("tets", input);
+    city = document.getElementById('search').value;
+    const myRequest = fetch(`https://api.weatherbit.io/v2.0/forecast/daily?city=${city},NC&key=${API_KEY}`);
+    myRequest.then(
+        response => {
+             if (response.status === 200) {
+                    response.json().then(data => {
+                        if(ul.lastChild){
+                            deleteChild('ul');
+                        }
+                        for (let i = 0; i < 16; i++){
+                            const newli = document.createElement('li')
+                            const date = new Date(data['data'][String(i)]['datetime'])
+                            newli.innerHTML = date.toLocaleDateString("fr", option) + 
+                            "<br><br>" + data['data'][String(i)]['temp'] + '°';
+                            ul.appendChild(newli);
+                        }
+                        defaultLogo.classList.add('hidden');
+                        cityName.textContent = city.toUpperCase();
+                    })
+            } else {
+                cityName.textContent = 'Aucune ville ne correspond à votre requete ';
+                deleteChild('ul');
+                defaultLogo.classList.remove('hidden');
+            }
+        })
 }
+
+btnSearch.addEventListener('click', getSearch);
